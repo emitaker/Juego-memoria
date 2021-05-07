e1dc573f158f81fa2b300e70d7b6b58d9d04c387
LorenaPalominoCastillo <65177726+LorenaPalominoCastillo@users.noreply.github.com>
2021-05-07 15:04:11 -0500

    Se centraron los numeros y se añadio un contador de tabs

--- memoria.py
+++ memoria.py
@@ -1,3 +1,7 @@
+#Codigo modicado por:
+#Autor: Emilio Campuzano Mejia
+#Autora: Lorena Palomino Castillo
+
 from random import *
 from turtle import *
 from freegames import path
@@ -6,9 +10,12 @@ car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
+taps = 0
 
 def square(x, y):
-    "Draw white square with black outline at (x, y)."
+    """
+    Draw white square with black outline at (x, y).
+    """
     up()
     goto(x, y)
     down()
@@ -19,28 +26,43 @@ def square(x, y):
         left(90)
     end_fill()
 
+
 def index(x, y):
-    "Convert (x, y) coordinates to tiles index."
+    """
+    Convert (x, y) coordinates to tiles index.
+    """
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
+
 def xy(count):
-    "Convert tiles count to (x, y) coordinates."
+    """
+    Convert tiles count to (x, y) coordinates.
+    """
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
+
 def tap(x, y):
-    "Update mark and hidden tiles based on tap."
+    """
+    Update mark and hidden tiles based on tap.
+    """
+    global taps
     spot = index(x, y)
     mark = state['mark']
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
+        taps += 1
+        print(taps)
+
     else:
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
 
 def draw():
-    "Draw image and tiles."
+    """
+    Draw image and tiles.
+    """
     clear()
     goto(0, 0)
     shape(car)
@@ -56,12 +78,12 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x + 27, y)
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
-
+        write(tiles[mark], font=('Arial', 30, 'normal'),align='center')
     update()
     ontimer(draw, 100)
+
 
 shuffle(tiles)
 setup(420, 420, 370, 0)
